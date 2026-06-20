4eb361267e8cebd3d60914e523968a16e00f397b
okinamo-o <louayhamdi438@gmail.com>
2026-06-20 15:00:20 +0100

    chore: enable debug mode in Sentry to diagnose missing feedback widget

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -8,7 +8,7 @@ Sentry.init({
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  debug: true,
 
   integrations: [
     Sentry.feedbackIntegration({
