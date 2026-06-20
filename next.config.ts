f581218973fae8ba6635c9bc24b0c42dfecd8ade
okinamo-o <louayhamdi438@gmail.com>
2026-06-20 14:52:43 +0100

    fix: remove deprecated Sentry plugin options that cause type errors during Next.js build

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,4 @@ export default withSentryConfig(nextConfig, {
   org: process.env.SENTRY_ORG,
   project: process.env.SENTRY_PROJECT,
   silent: !process.env.CI,
-  widenClientFileUpload: true,
-  hideSourceMaps: true,
 });
